4cbac5bb4027d9cda4d59c5e0b733f198f82d8f1
Heitor Guerra Carneiro <heitorgcarneiro@gmail.com>
2023-05-13 23:11:06 -0300

    feat: add zap logger for google cloud

--- modules/web/routerchi/internal/infra/logging/zap.go
+++ modules/web/routerchi/internal/infra/logging/zap.go
@@ -1,22 +1,60 @@
 package logging
 
 import (
-	"log"
-	"time"
-
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func GetLogger() *zap.SugaredLogger {
-	loggerConfig := zap.NewProductionConfig()
-	loggerConfig.EncoderConfig.TimeKey = "timestamp"
-	loggerConfig.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
+func encodeLevel() zapcore.LevelEncoder {
+	return func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		switch l {
+		case zapcore.DebugLevel:
+			enc.AppendString("DEBUG")
+		case zapcore.InfoLevel:
+			enc.AppendString("INFO")
+		case zapcore.WarnLevel:
+			enc.AppendString("WARNING")
+		case zapcore.ErrorLevel:
+			enc.AppendString("ERROR")
+		case zapcore.DPanicLevel:
+			enc.AppendString("CRITICAL")
+		case zapcore.PanicLevel:
+			enc.AppendString("ALERT")
+		case zapcore.FatalLevel:
+			enc.AppendString("EMERGENCY")
+		}
+	}
+}
 
-	logger, err := loggerConfig.Build()
-	if err != nil {
-		log.Fatal(err)
+// Based on:
+// https://cloud.google.com/logging/docs/structured-logging
+// https://github.com/uber-go/zap/issues/1095
+func GetLogger() *zap.SugaredLogger {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "severity",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    encodeLevel(),
+		EncodeTime:     zapcore.RFC3339TimeEncoder,
+		EncodeDuration: zapcore.MillisDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
-	return logger.Sugar()
+	loggerCfg := &zap.Config{
+		Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		Encoding:         "json",
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	plain, err := loggerCfg.Build(zap.AddStacktrace(zap.DPanicLevel))
+	if err != nil {
+		plain = zap.NewNop()
+	}
+	return plain.Sugar()
 }
