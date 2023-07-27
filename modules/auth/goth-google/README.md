9b642479f28638cd0371034ed2f5b843f5d31d69
Heitor Carneiro <heitorgcarneiro@gmail.com>
2023-07-27 19:08:36 -0300

    add example with google auth

---
+++ modules/auth/goth-google/README.md
@@ -0,0 +1,30 @@
+# Discovery of Google OAuth 2.0 with goth Package
+
+
+```
+cp .env.example .env
+
+go mod tidy
+
+go run main.go
+```
+
+
+## Docs
+
+https://support.google.com/cloud/answer/6158849?hl=en#zippy=
+
+https://support.google.com/workspacemigrate/answer/9222992?hl=en
+
+https://github.com/markbates/goth
+
+https://gobuffalo.io/documentation/guides/goth/
+
+https://www.loginradius.com/blog/engineering/twitter-authentication-with-golang-and-goth/
+
+https://wathsara.medium.com/asgardeo-authentication-with-golang-and-goth-2be8eea7dbe7
+
+https://github.com/Wathsara/asgardeo-go-sample-app/blob/main/main.go
+
+https://github.com/markbates/goth/issues/58
+
---
+++ modules/auth/goth-google/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+
+	"github.com/gorilla/pat"
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/bitbucket"
+	"github.com/markbates/goth/providers/github"
+	"github.com/markbates/goth/providers/google"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// hashKey is required, used to authenticate values using HMAC
+	hashKey := securecookie.GenerateRandomKey(32)
+	if hashKey == nil {
+		log.Fatal("Fail to generate key")
+	}
+
+	// blockKey is optional, used to encrypt values
+	blockKey := securecookie.GenerateRandomKey(32)
+	if hashKey == nil {
+		log.Fatal("Fail to generate key")
+	}
+
+	maxAge := 86400 * 30 // 30 days
+	isProd := false      // Set to true when serving over https
+
+	store := sessions.NewCookieStore(hashKey, blockKey)
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(os.Getenv("GOOGLE_KEY"), os.Getenv("GOOGLE_SECRET"), "http://localhost:3000/auth/google/callback"),
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+		bitbucket.New(os.Getenv("BITBUCKET_KEY"), os.Getenv("BITBUCKET_SECRET"), "http://localhost:3000/auth/bitbucket/callback"),
+	)
+
+	m := map[string]string{
+		"bitbucket": "Bitbucket",
+		"github":    "Github",
+		"google":    "Google",
+	}
+
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+
+	p := pat.New()
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			fmt.Fprintln(res, err)
+			return
+		}
+		t, _ := template.New("foo").Parse(userTemplate)
+		t.Execute(res, user)
+	})
+
+	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.Logout(res, req)
+		res.Header().Set("Location", "/")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		// try to get the user without re-authenticating
+		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
+			t, _ := template.New("foo").Parse(userTemplate)
+			t.Execute(res, gothUser)
+		} else {
+			gothic.BeginAuthHandler(res, req)
+		}
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t, _ := template.New("foo").Parse(indexTemplate)
+		t.Execute(res, providerIndex)
+	})
+
+	log.Println("listening on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", p))
+}
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
+}
+
+var indexTemplate = `{{range $key,$value:=.Providers}}
+    <p><a href="/auth/{{$value}}">Log in with {{index $.ProvidersMap $value}}</a></p>
+{{end}}`
+
+var userTemplate = `
+<p><a href="/logout/{{.Provider}}">logout</a></p>
+<p>Name: {{.Name}} [{{.LastName}}, {{.FirstName}}]</p>
+<p>Email: {{.Email}}</p>
+<p>NickName: {{.NickName}}</p>
+<p>Location: {{.Location}}</p>
+<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></p>
+<p>Description: {{.Description}}</p>
+<p>UserID: {{.UserID}}</p>
+<p>AccessToken: {{.AccessToken}}</p>
+<p>ExpiresAt: {{.ExpiresAt}}</p>
+<p>RefreshToken: {{.RefreshToken}}</p>
+`
