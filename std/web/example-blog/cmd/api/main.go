a9c7ce7cb9afb047fe5643ba700aeea211e3e37d
Heitor Carneiro <heitor.carneiro@contabilizei.com.br>
2024-09-12 20:18:12 -0300

    feat: init project

---
+++ std/web/example-blog/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"example-blog/internal/server"
+	"fmt"
+)
+
+func main() {
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
---
+++ std/web/example-blog/go.mod
@@ -0,0 +1,9 @@
+module example-blog
+
+go 1.23.1
+
+require (
+	github.com/a-h/templ v0.2.778
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.23
+)
